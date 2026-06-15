8c3fb8ef18d0b0840df180c3e8d3ce7877a581ca
Abanoub Kerols <abanoubkerols@gmail.com>
2026-06-15 11:39:47 +0300

    URL shortening and redirect endpoints

---
+++ urlShortener/src/config/index.ts
@@ -0,0 +1,11 @@
+export default () => ({
+  host: process.env.HOST,
+  environment: process.env.NODE_ENV || `development`,
+  redis: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD,
+  },
+  apiKey: process.env.API_KEY,
+});
